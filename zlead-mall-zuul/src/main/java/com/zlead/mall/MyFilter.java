2cf0cfa965cc04a0503ecececc3b485bec60b151
ch18505657339 <1650611030@>
2019-03-25 14:54:26 +0800

    未全部提交

---
+++ zlead-mall-zuul/src/main/java/com/zlead/mall/MyFilter.java
@@ -0,0 +1,51 @@
+package com.zlead.mall;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+	private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+		Object accessToken = request.getParameter("token");
+		String refer=request.getHeader("refer");
+	
+		if (accessToken != null) {
+			return null;
+		}
+		log.warn("token is empty");
+		ctx.setSendZuulResponse(false);
+		ctx.setResponseStatusCode(401);
+		try {
+			ctx.getResponse().getWriter().write("token is empty");
+		} catch (Exception e) {
+		}
+		return null;
+
+	}
+}
