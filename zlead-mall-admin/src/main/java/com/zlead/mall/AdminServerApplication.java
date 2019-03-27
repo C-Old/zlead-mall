147a54c43c00f0386b12d3f2afb4e57ac858f49e
ch18505657339 <1650611030@>
2019-03-27 17:14:52 +0800

    注册zuul测试完成

--- zlead-mall-admin/src/main/java/com/zlead/mall/AdminServerApplication.java
+++ zlead-mall-admin/src/main/java/com/zlead/mall/AdminServerApplication.java
@@ -3,8 +3,10 @@ package com.zlead.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan({"com.zlead.mall.mapper","com.zlead.mall.dao"})
 public class AdminServerApplication {
     public static void main(String[] args) {
